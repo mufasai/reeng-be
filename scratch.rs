df7d8e348571e527466dee830e946f0f5d355979
mufasai <musafadil03@gmail.com>
2026-04-22 23:19:08 +0700

    fix connecting

--- scratch.rs
+++ scratch.rs
@@ -1,24 +1,9 @@
-use surrealdb::engine::local::Mem;
+use surrealdb::engine::remote::http::{Client, Http};
 use surrealdb::Surreal;
-use serde_json::Value;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let db = Surreal::new::<Mem>(()).await?;
-    db.use_ns("test").use_db("test").await?;
-    
-    let query = r#"
-        CREATE test CONTENT {
-            id: type::thing('test', '123'),
-            val: 'hello'
-        };
-        CREATE test:456 CONTENT { val: 'world' };
-    "#;
-    
-    let mut response = db.query(query).await?;
-    let r1: Vec<Value> = response.take(0)?;
-    println!("R1: {:?}", r1);
-    let r2: Vec<Value> = response.take(1)?;
-    println!("R2: {:?}", r2);
-    Ok(())
+async fn main() {
+    println!("Connecting...");
+    let db = Surreal::new::<Http>("127.0.0.1:9999").await;
+    println!("Result: {:?}", db.is_ok());
 }
